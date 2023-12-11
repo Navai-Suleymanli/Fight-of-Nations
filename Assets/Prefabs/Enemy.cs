65330531d7d14507c8b6530557021ce872d56cb9
Khatai Suleymanli <80199997+Khatai-Suleymanli@users.noreply.github.com>
2023-12-11 19:32:07 +0200

    added comments

--- Assets/Prefabs/Enemy.cs
+++ Assets/Prefabs/Enemy.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -26,18 +27,20 @@ public class Enemy : MonoBehaviour
 
     void Update()
     {
+        // occupation ststem  ----------------------------------
         isOccupation = Physics.CheckSphere(transform.position, distance, groundLayer);
         if (isOccupation && !isDecreasing)
         {
             StartCoroutine(DecreaseValue());
         }
+        // -----------------------------------------------------
     }
 
-    private void FixedUpdate()
+    private void FixedUpdate() 
     {
-        if (currentTargetBarrier != null)
+        if (currentTargetBarrier != null) // if currenttarget barrier is defined(not null)
         {
-            float distanceToCurrentBarrier = Vector3.Distance(transform.position, currentTargetBarrier.transform.position);
+            float distanceToCurrentBarrier = Vector3.Distance(transform.position, currentTargetBarrier.transform.position); // calculating distace to teh current barrier
             if (distanceToCurrentBarrier <= stopDistance)
             {
                 agent.isStopped = true;
@@ -49,31 +52,33 @@ public class Enemy : MonoBehaviour
         }
         else
         {
-            GoToRandomDestination();
+            GoToRandomDestination();  //this will set a barrier to the currentactive barrier. that abrrier will be chosen randomly from the list of active barriers
         }
     }
 
     private void GoToRandomDestination()
     {
-        if (currentTargetBarrier != null && currentTargetBarrier.enemyCount < 2) return;
+        if (currentTargetBarrier != null && currentTargetBarrier.enemyCount < 2) return;  // if curretn barrer has place to go, then do nothing
 
-        List<Barrier> validBarriers = new List<Barrier>();
+        List<Barrier> validBarriers = new List<Barrier>(); // list of valid(not full) barriers
         foreach (GameObject destination in destinations)
         {
-            Barrier barrier = destination.GetComponent<Barrier>();
-            if (barrier != null && barrier.CanAddEnemy())
+            Barrier barrier = destination.GetComponent<Barrier>(); // getting individual abrrier component of each barrier in the array
+            if (barrier != null && barrier.CanAddEnemy())  // if the chosen barrier has a barrier script, and is not full
             {
-                validBarriers.Add(barrier);
+                validBarriers.Add(barrier);  // add that barrier to the list of valid barriers
             }
         }
 
-        if (validBarriers.Count > 0)
+        if (validBarriers.Count > 0)  // as long as there are valid barrier(s)
         {
-            Barrier selectedBarrier = validBarriers[Random.Range(0, validBarriers.Count)];
+            Barrier selectedBarrier = validBarriers[Random.Range(0, validBarriers.Count)]; // rondomly choose a barrier from the list of valid barriers
             agent.SetDestination(selectedBarrier.transform.position);
-            currentTargetBarrier?.RemoveEnemy(); // Remove from old barrier
+            currentTargetBarrier?.RemoveEnemy(); // Remove from old barrier when enemy changes the destination. 
+            //If you don't remove the enemy from the currentTargetBarrier, the enemy might be counted as being at two barriers at once. 
+
             currentTargetBarrier = selectedBarrier;
-            currentTargetBarrier.AddEnemy();
+            currentTargetBarrier.AddEnemy();  // add enemy safely when chosen the current barrier
             agent.isStopped = false;
         }
         else
