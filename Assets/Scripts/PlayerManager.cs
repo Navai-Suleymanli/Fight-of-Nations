f9447657eee93bb1fe284c546cbaf9b19bbf3b95
Khatai Suleymanli <80199998+Navai-Suleymanli@users.noreply.github.com>
2024-01-10 02:12:39 +0400

    Revert "Merge branch 'main' of https://github.com/Navai-Suleymanli/Fight-of-Nations"
    This reverts commit 499669043dc00baa1a8727f9320fa5ae52a82a42, reversing
    changes made to 32deab1e95e1294228ee086128394995556d65f7.

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -2,18 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UI;
+
 
 
 public class PlayerManager : MonoBehaviour
 {
 
-    
+    [SerializeField] private int playerHealth = 100;
     private AudioSource audioSource;
     [SerializeField] AudioClip playerAhhVoice;
-    public Image healthBar;
-    [SerializeField] private int playerHealth = 100;
-
 
     // Start is called before the first frame update
     void Start()
@@ -28,10 +25,8 @@ public class PlayerManager : MonoBehaviour
     }
 
     public void TakeDamage(int damage)
-
     {
         playerHealth -= damage;
-        healthBar.fillAmount = playerHealth / 100f;
         audioSource.PlayOneShot(playerAhhVoice, 1f);
         if (playerHealth <= 0)
         {
