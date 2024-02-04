c70ba45bdcffbfc8b09a3565ab50e9d4c33059a5
Khatai Suleymanli <80199997+Khatai-Suleymanli@users.noreply.github.com>
2024-02-04 18:18:34 +0200

    son deyisiklikler

--- Assets/Scripts/SmoothGo.cs
+++ Assets/Scripts/SmoothGo.cs
@@ -9,16 +9,49 @@ using UnityEngine;
  * This script should be attached to the left and right hand 
  * in the IK Rig object
 */
+
+
 public class SmoothGo : MonoBehaviour
 {
+
+    WeaponController wc;
     [SerializeField]
-    private Transform _target; // Where the hands need to go
+    private Transform _target;
+    [SerializeField]
+    private Transform _target_makarov;
+    [SerializeField]
+    private Transform _target_sniper;
+    // Where the hands need to go
     [SerializeField]
     private float _speed;
 
+    void Start()
+    {
+        wc = gameObject.GetComponentInParent<WeaponController>();
+
+    }
+
     private void LateUpdate()
     {
-        transform.position = Vector3.Lerp(this.transform.position, _target.transform.position, _speed * Time.deltaTime);
-        transform.rotation = Quaternion.Euler(_target.transform.rotation.eulerAngles);
+        if (wc == null)
+        {
+            Debug.Log("NULLL");
+        }
+        if (wc.AKM)
+        {
+            transform.position = Vector3.Lerp(this.transform.position, _target.transform.position, _speed * Time.deltaTime);
+            transform.rotation = Quaternion.Euler(_target.transform.rotation.eulerAngles);
+        }
+        if (wc.Sniper)
+        {
+            transform.position = Vector3.Lerp(this.transform.position, _target_sniper.transform.position, _speed * Time.deltaTime);
+            transform.rotation = Quaternion.Euler(_target_sniper.transform.rotation.eulerAngles);
+        }
+        if (wc.isMakarov)
+        {
+            transform.position = Vector3.Lerp(this.transform.position, _target_makarov.transform.position, _speed * Time.deltaTime);
+            transform.rotation = Quaternion.Euler(_target_makarov.transform.rotation.eulerAngles);
+        }
     }
+       
 }
