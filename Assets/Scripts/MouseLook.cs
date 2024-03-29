090f03f2c981aa51e0d64f832ca4b8fde733e551
Khatai Suleymanli <80199998+Navai-Suleymanli@users.noreply.github.com>
2024-03-29 22:34:18 +0200

    mouse look

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -1,61 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
-    [SerializeField]
-    [Range(0, 2)]
-    private float sensitivity = 100f;
+    public Transform Target;
+    public float MouseSensitivity = 10f;
 
-    public Camera cam;
-    float xRotation = 0f;
+    private float verticalRotation;
+    private float horizontalRotation;
 
-    private Vector2 smoothInput = Vector2.zero;
-    private Vector2 currentInput = Vector2.zero;
-    private Vector2 inputVelocity = Vector2.zero;
-
-
-    //light //////
-
-    public Light spotLight;
-    //public GameObject gun;
-
-
-
-    Vector3 followCam()
+    void LateUpdate()
     {
-        return spotLight.transform.position = cam.gameObject.transform.position + new Vector3(0f, 0f, 0.5f);
+        if (Target == null)
+        {
+            return;
+        }
 
-    }
+        transform.position = Target.position + new Vector3(0, 3.15f, 0);
 
-    /* Vector3 followCamGun()
-     {
-         return gun.transform.position = gameObject.transform.position + new Vector3(0f, 0.1f, 0.5f);
-     }*/
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
 
-    void Start()
-    {
-        Cursor.lockState = CursorLockMode.Locked;
-    }
+        verticalRotation -= mouseY * MouseSensitivity;
+        verticalRotation = Mathf.Clamp(verticalRotation, -70f, 70f);
 
-    void Update()
-    {
-        // Smooth mouse input
-        currentInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
-        smoothInput = Vector2.SmoothDamp(smoothInput, currentInput, ref inputVelocity, 0.08f);
+        horizontalRotation += mouseX * MouseSensitivity;
 
-        xRotation -= smoothInput.y * sensitivity;
-        xRotation = Mathf.Clamp(xRotation, -90f, 60f);
-
-        cam.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-
-        transform.Rotate(Vector3.up * smoothInput.x * sensitivity);
-
-        followCam();
-        // followCamGun();
-
-        spotLight.transform.rotation = Quaternion.Slerp(spotLight.transform.rotation, cam.transform.rotation, 0.1f);
-        //gun.transform.rotation = Quaternion.Slerp(gun.transform.rotation, cam.transform.rotation, 0.1f);
+        transform.rotation = Quaternion.Euler(verticalRotation, horizontalRotation, 0);
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,11 +8,10 @@ public class PlayerMovement : NetworkBehaviour
 {
 
     [Networked] public NetworkButtons ButtonsPrevious { get; set; }  //----------------------------------------------------------------------------
+    public Camera Camera;
 
 
 
-
-    public Vector3 move;
     // CharacterController controller;
     [SerializeField] private float walkSpeed = 0f;
     [SerializeField] private float sprintSpeed = 0f;
@@ -29,33 +28,29 @@ public class PlayerMovement : NetworkBehaviour
     [SerializeField] AudioClip playerRunningBreath;
     private bool isAudioPlaying = false;
 
-    private float gravityValue = -9.81f; // Gravity value
-    private float verticalVelocity = 0f; // Vertical velocity due to gravity
-
     // Camera bobbing effect variables
     public bool pressedAtTheSameTime = false;
     public float speed = 0f;
 
 
     private NetworkCharacterController _cc;
-    private bool _jumpPressed;
 
-    private void Awake()
+    public override void Spawned()
     {
         _cc = GetComponent<NetworkCharacterController>();
-        weapon = GetComponent<WeaponController>();
-        audioSource = GetComponent<AudioSource>();
-    }
-
-    void Update()
-    {
-        if (Input.GetButtonDown("Jump"))
+        if (HasStateAuthority)
         {
-            _jumpPressed = true;
+            Camera = Camera.main;
+            Camera.GetComponent<MouseLook>().Target = transform;
         }
     }
 
-
+    private void Awake()
+    {
+       
+        weapon = GetComponent<WeaponController>();
+        audioSource = GetComponent<AudioSource>();
+    }
 
     // Update is called once per frame
     public override void FixedUpdateNetwork()
@@ -77,21 +72,29 @@ public class PlayerMovement : NetworkBehaviour
         // store latest input as 'previous' state we had
         ButtonsPrevious = input.buttons;
 
-        // movement (check for down)
+
+        // movement (check for down) //-----------------------------------------------------------------------------------------
         var move_vector = default(Vector3);
+        Quaternion cameraRotationY = Quaternion.Euler(0, Camera.transform.rotation.eulerAngles.y, 0);
+
         //var jump_vector = default(Vector3);
+
+
 
         if (input.buttons.IsSet(MyButtons.Forward)) { move_vector.z += 1; }
         if (input.buttons.IsSet(MyButtons.Backward)) { move_vector.z -= 1; }
 
         if (input.buttons.IsSet(MyButtons.Left)) { move_vector.x -= 1; }
         if (input.buttons.IsSet(MyButtons.Right)) { move_vector.x += 1; }
-        
+        if (input.buttons.IsSet(MyButtons.Jump)) { _cc.Jump(); }
+
+
+        Vector3 move_teze = cameraRotationY * move_vector;//-------------------------------------------------
+
 
         //jumping-----------------------
-       if(input.buttons.IsSet(MyButtons.Jump)) { _cc.Jump(); }
 
-        DoMove(move_vector);
+        DoMove(move_teze);
 
 
         // Check for running input
