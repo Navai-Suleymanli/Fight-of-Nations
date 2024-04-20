fda339e69e4ce910e48c73076044e11dff0ead4e
Khatai Suleymanli <80199998+Navai-Suleymanli@users.noreply.github.com>
2024-04-21 01:37:32 +0300

    Revert "jumping added"
    This reverts commit 215bb85c8f9c626579e25428b1754299f5ed94aa.

--- Assets/Scripts/Fusion/NetworkInputData.cs
+++ Assets/Scripts/Fusion/NetworkInputData.cs
@@ -1,18 +1,7 @@
-
 using Fusion;
 using UnityEngine;
 
-enum MyButtons//----------------------------------------------------------------
-{
-    Forward = 0,
-    Backward = 1,
-    Left = 2,
-    Right = 3,
-    Jump = 4,
-}//----------------------------------------------------------------------------
-
 public struct NetworkInputData : INetworkInput
 {
-    public NetworkButtons buttons;
     public Vector3 direction;
 }
